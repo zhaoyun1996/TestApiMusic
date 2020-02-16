83eae4fdc6347dadaa7186f551570c4c625bc4a0
Vũ Văn Kiệt <42272455+zhaoyun1996@users.noreply.github.com>
2020-02-16 12:19:18 +0700

    a
    a

--- core/Chatfuel.php
+++ core/Chatfuel.php
@@ -19,7 +19,11 @@ class Chatfuel
   {
     if (count($this->response) > 0) {
       try {
-        header('Content-Type: application/json');
+//         header('Content-Type: application/json');
+        header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
+        header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+        header('Access-Control-Max-Age: 1000');
+        header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
         echo json_encode(array('messages' => $this->response));
         exit;
       } catch (Exception $e) {
