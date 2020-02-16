f2c3cf5ec90fb867122afa468be741ba1204a903
zhaoyun1996 <kiet14020248uet@gmail.com>
2020-02-16 13:09:52 +0700

    Update zing-webhook.php

--- zing-webhook.php
+++ zing-webhook.php
@@ -42,7 +42,7 @@
    
        $name = $data['data']['name'];
        $artists_names = $data['data']['artists_names'];
-       $chatfuel->sendTextSuccess($text, $artists_names);
+       $chatfuel->sendTextSuccess($name, $artists_names);
        $chatfuel->sendImage($data['data']['artist']['cover']); 
        $chatfuel->sendAudio(str_replace('//', 'https://', $data['data']['source']['128']));
    } else {
